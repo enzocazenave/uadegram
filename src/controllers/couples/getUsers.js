f6f049a49ca56dae540f03147c59a55f4815a010
Enzo Cazenave <encaze24@hotmail.com>
2023-05-01 23:34:13 -0300

    Add new filter in get users for couple screen

--- src/controllers/couples/getUsers.js
+++ src/controllers/couples/getUsers.js
@@ -7,10 +7,10 @@ const getUsers = async (req, res = response) => {
     const { limit } = req.query;
 
     try {
-        const users = await User.find({ gender: 0 }).skip(limit - 10).limit(limit);
         const user = await User.findById(id);
+        const users = await User.find({ gender: user.genderYouSearch }).skip(limit - 10).limit(limit);
 
-        const refactoredUsers = users.map(({ _doc: { _id, name, profile_images, about, birthdate, isOnline } }) => {
+        const refactoredUsers = users.map(({ _doc: { _id, name, profile_images, about, birthdate, isOnline, genderYouSearch } }) => {
 
             const profileImages = profile_images.filter(image => !image.stored);
 
@@ -21,12 +21,14 @@ const getUsers = async (req, res = response) => {
                 about,
                 birthdate,
                 isOnline,
+                genderYouSearch
             }
         }).filter((refactoredUser) => (
-            user._id.toString() != refactoredUser._id.toString() &&
-            refactoredUser.profileImages.length > 0 &&
-            !(user.matches.includes(refactoredUser._id)) &&
-            !(user.noMatches.includes(refactoredUser._id))
+            user._id.toString() != refactoredUser._id.toString() && // QUE LA ID DEL USUARIO SEA DISTINTA DEL QUE REALIZA LA PETICION
+            refactoredUser.profileImages.length > 0 && // QUE EL USUARIO TENGA IMAGENES SUBIDAS A SU PERFIL
+            !(user.matches.includes(refactoredUser._id)) && // QUE EL USUARIO NO TENGA LIKE DEL QUE REALIZA LA PETICION
+            !(user.noMatches.includes(refactoredUser._id)) && // QUE EL USUARIO NO TENGA UN DISLIKE DEL QUE REALIZA LA PETICION
+            user.gender == refactoredUser.genderYouSearch // QUE EL GENERO QUE BUSCA EL USUARIO SEA EL DEL QUE REALIZA LA PETICION
         ));
 
         res.status(200).json({
